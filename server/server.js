4504ed41f052ef69380dfa83ee92d06bf4a54717
AJ Zawawi <aminojzawawi@gmail.com>
2016-08-04 22:43:28 -0400

    (feat) Complete initial server setup and routes.

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const middleware = './middleware/middleware';
+const routes = './routes/routes';
+
+const app = express();
+app.use(cors());
+
+// Routes and middleware
+require('./middleware/middleware')(app, express);
+require('./routes/routes')(app, express);
+
+const port = process.env.PORT || 3000;
+
+// For development, webpack willbe used
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack');
+  const config = require('../webpack.config.js');
+  const compiler = webpack(config);
+}
+
+app.listen(port, function() {
+  console.log('You are now connected to ', port);
+})
+
+module.exports = app;
