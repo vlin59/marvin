ed9bb5b7e33729cb2803fe5bc034511fb5fbb973
Taylor Page <jonathantaylorpage@gmail.com>
2016-08-05 17:16:21 -0500

    create search events action

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,8 +1,16 @@
 export const SET_EVENTS = 'SET_EVENTS';
+export const SEARCH_EVENTS = 'SEARCH_EVENTS'
 
 export function setEvents (event) {
   return {
     type: SET_EVENTS,
+    payload: event
+  };
+}
+
+export function searchEvents (event) {
+  return {
+    type: SEARCH_EVENTS,
     payload: event
   };
 }
