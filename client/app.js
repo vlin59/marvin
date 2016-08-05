3624eaff1716584dca83d95d26a8b35f4f9488ef
Ji Baik <jiwoongb@gmail.com>
2016-08-04 18:24:07 -0700

    (feat) Add react-stormpath modules

--- client/app.js
+++ client/app.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
+import { browserHistory } from 'react-router';
+import ReactStormpath, { Router, AuthenticatedRoute, LoginLink } from 'react-stormpath';
 import { applyMiddleware, createStore } from 'redux';
 import routes from './routes';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactStormpath.init();
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
--- client/routes.js
+++ client/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link, hashHistory, browserHistory, IndexRoute, Redirect} from 'react-router';
+import { Route, Link, hashHistory, browserHistory, IndexRoute, Redirect} from 'react-router';
 import MainPage from './components/main';
 import EventsPage from './containers/EventsPage';
 
