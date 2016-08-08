2908ac13053679790670a1d5604316a4ada6f116
Taylor Page <jonathantaylorpage@gmail.com>
2016-08-08 15:08:27 -0500

    (chore) create search helper function

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -1,3 +1,4 @@
+const request = require('request');
 /* All Helper Functions Belong Here
    Use callbacks to return values back to the routes */
 
@@ -6,4 +7,10 @@ exports.getYelpToken = function(cb){
 }
 
 exports.queryYelp = function(query, cb){
+}
+
+exports.searchEventBrite = function(category, query) {
+  request('https://www.eventbriteapi.com/v3/events/search/?categories=' + category + '&q=' + query + '&token=UQOCU57TT67WA4W7V6RE', function(error, res, req) {
+    cb(JSON.parse(res.body));
+  });
 }
