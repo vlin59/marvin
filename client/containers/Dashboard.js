fb00bb418f59719f2f459a58bf21f037509bc908
Taylor Page <jonathantaylorpage@gmail.com>
2016-08-09 17:02:03 -0500

    (chore) create array of objects to represent dashboard components

--- client/containers/Dashboard.js
+++ client/containers/Dashboard.js
@@ -3,7 +3,15 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 const comps = [
-  'interests', 'calendar', 'todo', 'fitbit', 'weather'
+  { title: 'saved_events',      component: '' },
+  { title: 'reminders',         component: '' },
+  { title: 'weather',           component: '' },
+  { title: 'interests',         component: '' },
+  { title: 'todo',              component: '' },
+  { title: 'calendar',          component: '' },
+  { title: 'wellness_tracker',  component: '' },
+  { title: 'arduino',           component: '' },
+  { title: 'payment_reminders', component: '' }
 ]
 
 class Dashboard extends React.Component{
@@ -14,8 +22,10 @@ class Dashboard extends React.Component{
   render() {
     return(
       <div>
+        <h1>Dashboard</h1>
+        <div class='marvin'>Marvin</div>
         {
-          comps.map(comp => <div class={comp}>{comp}</div>)
+          comps.map(comp => <div class={comp.title}>{comp.title}</div>)
         }
       </div>
     )
