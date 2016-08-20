250186ad74a3d8779d18f8aaf45ebd809bb43a54
AJ Zawawi <aminojzawawi@gmail.com>
2016-08-19 20:30:02 -0400

    (fix) Persist token and update google helpers

--- server/db/controllers/tokenController.js
+++ server/db/controllers/tokenController.js
@@ -1,19 +1,39 @@
 const GoogleToken = require('../models/googleToken');
+const request = require('request');
 
 exports.storeToken = function(token){
 
-  var token = new GoogleToken({
-    token: token
+  GoogleToken.findOneAndUpdate({}, {token: token}, {upsert:true}, function(err, doc){
+    if(err){ console.log(err); }
   });
 
-  token.save(function(err){
-    if (err) { return next(err); }
-  });
-
-  console.log(token);
 }
 
-exports.checkToken = function(){
+exports.checkToken = function(cb){
 
+  GoogleToken.findOne({}, function (err, token){
+    if(!token) {
+      cb(null);
+    } else {
+
+      const url = "https://www.googleapis.com/oauth2/v1/tokeninfo"
+      var opts = {
+        uri:url,
+        qs: {
+          access_token: token.token.access_token
+        }
+      }
+
+      request(opts, function(err, res, body){
+        const isValidToken = JSON.parse(body);
+        if(isValidToken.expires_in && isValidToken.expires_in > 50){
+          cb(token.token);
+        } else {
+          cb(null);
+        }
+      });
+    }
+
+  });
 
 }
