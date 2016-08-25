8312064283045716083c68ab10d0f8a445fce406
AJ Zawawi <aminojzawawi@gmail.com>
2016-08-25 01:03:07 -0400

    (chore) Remove category for event search

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,6 +3,7 @@ const google = require('../helpers/google');
 const lights = require('../helpers/lights');
 const twilio = require('../helpers/twilio');
 const weather = require('../helpers/weather');
+const marvin = require('../helpers/marvin');
 const stormpath = require('express-stormpath');
 const userController = require('../db/controllers/userController');
 const mailer = require('express-mailer');
@@ -11,6 +12,16 @@ const eventItemsController = require('../db/controllers/eventItemsController');
 
 
 module.exports = function(app, express) {
+
+  /* Marvin Routes */
+
+  app.post('/marvin/query', function(req, res){
+    const query = req.body.params.query;
+
+    marvin.classifyQuery(query, function(result){
+      res.send(result);
+    });
+  });
 
   /* Database Routes */
   app.post('/api/user/save', stormpath.loginRequired, userController.createOrFindUser);
@@ -59,11 +70,15 @@ module.exports = function(app, express) {
 
   app.post('/api/eventbrite', function(req, res){
     var query = req.body.query;
-    var category = req.body.category;
+    // var category = req.body.category;
     var lat = req.body.lat;
     var long = req.body.long;
 
-    helpers.searchEventBrite(category, query, lat, long, function(data) {
+    // helpers.searchEventBrite(category, query, lat, long, function(data) {
+    //   res.send(201, data);
+    // });
+
+    helpers.searchEventBrite(query, lat, long, function(data) {
       res.send(201, data);
     });
   });
@@ -78,7 +93,30 @@ module.exports = function(app, express) {
   // Pass a type as part of request
   // Example "restaurants", "bars", "clubs" etc
   app.post('/api/yelp', function(req, res){
-    res.send(200);
+    var lat = req.body.lat;
+    var long = req.body.long;
+    var query = req.body.query;
+
+    helpers.getYelpToken(function(token){
+
+      // Parse the token before adding it to the query
+      const parsedToken = JSON.parse(token);
+
+      // The query built using the request params
+      const search = {
+        token: parsedToken,
+        term: query,
+        lat: lat,
+        long: long
+      }
+
+      // This is a generic helper that can used for
+      // any Yelp query
+      helpers.queryYelp(search, function(data){
+        res.send(200, data);
+      });
+    });
+
   });
 
   // This route will handle all API queries for meetup
